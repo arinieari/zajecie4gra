e6d69e03353d3fa5c67ba2de821ed029189da46b
arinieari <162135664+arinieari@users.noreply.github.com>
2024-04-11 17:23:27 +0200

    Update flashlight.cs

--- Assets/flashlight.cs
+++ Assets/flashlight.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class flashlight : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Light2D light;
+    private bool isDark;
     void Start()
     {
-        
+        light = GetComponent<Light2D>();
+        light.enabled = false;
+        isDark = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("dark"))
+        {
+            light.enabled = true;
+            isDark = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (isDark && collision.gameObject.CompareTag("dark"))
+        {
+            light.enabled = false;
+            isDark = false;
+        }
     }
 }
